52b9f4af29c7494cbdb3452c6ac0a20e8a5ea2d0
tangxixi <1208495315@qq.com>
2019-07-01 23:34:55 +0800

    elementUI按需引入，vscode配置（stylus，模板）

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import element1 from './elementUI'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(element1)
 new Vue({
   router,
   store,
---
+++ src/views/test.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+<script>
+export default {
+  name: "xxx",
+  data () {
+    return {
+      
+    };
+  },
+  created () {
+    
+  },
+  mounted () {
+    
+  },
+  methods () {
+    
+  },
+}
+</script>
+<style lang="stylus" scoped>
+  
+</style>
